803d1e6799d6e751fdf7985c4c70364b4b5d6b4e
Никита Бокарев <tigrnik3@gmail.com>
2018-05-18 09:58:38 +0300

    v 1.5.2

--- Диплом/boxOffice/boxOffice/scheduleForm.Designer.cs
+++ Диплом/boxOffice/boxOffice/scheduleForm.Designer.cs
@@ -33,9 +33,19 @@
             this.deleteButton = new System.Windows.Forms.Button();
             this.editButton = new System.Windows.Forms.Button();
             this.schedulePanel = new System.Windows.Forms.Panel();
-            this.headerLabel = new System.Windows.Forms.Label();
             this.submitButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.perfomancesCombobox = new System.Windows.Forms.ComboBox();
+            this.perfomancesLabel = new System.Windows.Forms.Label();
+            this.theatreLabel = new System.Windows.Forms.Label();
+            this.theatreTextbox = new System.Windows.Forms.TextBox();
+            this.perfomanceDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.perfomanceDateLabel = new System.Windows.Forms.Label();
+            this.perfomanceStartsLabel = new System.Windows.Forms.Label();
+            this.perfomanceStartsDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.perfomanceEndsLabel = new System.Windows.Forms.Label();
+            this.perfomanceEndsDateTimePicker = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.scheduleDataGridView)).BeginInit();
             this.schedulePanel.SuspendLayout();
             this.SuspendLayout();
@@ -43,20 +53,20 @@
             // scheduleDataGridView
             // 
             this.scheduleDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.scheduleDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.scheduleDataGridView.Location = new System.Drawing.Point(9, 10);
+            this.scheduleDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.scheduleDataGridView.Name = "scheduleDataGridView";
             this.scheduleDataGridView.RowTemplate.Height = 24;
-            this.scheduleDataGridView.Size = new System.Drawing.Size(902, 298);
+            this.scheduleDataGridView.Size = new System.Drawing.Size(676, 242);
             this.scheduleDataGridView.TabIndex = 0;
             // 
             // addButton
             // 
             this.addButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(150)))));
             this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.addButton.Location = new System.Drawing.Point(12, 317);
-            this.addButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addButton.Location = new System.Drawing.Point(9, 258);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(185, 70);
+            this.addButton.Size = new System.Drawing.Size(139, 57);
             this.addButton.TabIndex = 2;
             this.addButton.Text = "Добавить";
             this.addButton.UseVisualStyleBackColor = false;
@@ -66,10 +76,9 @@
             // 
             this.deleteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(150)))));
             this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.deleteButton.Location = new System.Drawing.Point(398, 317);
-            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteButton.Location = new System.Drawing.Point(298, 258);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(185, 70);
+            this.deleteButton.Size = new System.Drawing.Size(139, 57);
             this.deleteButton.TabIndex = 6;
             this.deleteButton.Text = "Удалить";
             this.deleteButton.UseVisualStyleBackColor = false;
@@ -78,10 +87,9 @@
             // 
             this.editButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(150)))));
             this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.editButton.Location = new System.Drawing.Point(205, 317);
-            this.editButton.Margin = new System.Windows.Forms.Padding(4);
+            this.editButton.Location = new System.Drawing.Point(154, 258);
             this.editButton.Name = "editButton";
-            this.editButton.Size = new System.Drawing.Size(185, 70);
+            this.editButton.Size = new System.Drawing.Size(139, 57);
             this.editButton.TabIndex = 5;
             this.editButton.Text = "Редактировать";
             this.editButton.UseVisualStyleBackColor = false;
@@ -89,33 +97,32 @@
             // 
             // schedulePanel
             // 
+            this.schedulePanel.Controls.Add(this.perfomanceEndsLabel);
+            this.schedulePanel.Controls.Add(this.perfomanceEndsDateTimePicker);
+            this.schedulePanel.Controls.Add(this.perfomanceStartsLabel);
+            this.schedulePanel.Controls.Add(this.perfomanceStartsDateTimePicker);
+            this.schedulePanel.Controls.Add(this.perfomanceDateLabel);
+            this.schedulePanel.Controls.Add(this.perfomanceDateTimePicker);
+            this.schedulePanel.Controls.Add(this.theatreTextbox);
+            this.schedulePanel.Controls.Add(this.theatreLabel);
+            this.schedulePanel.Controls.Add(this.perfomancesLabel);
+            this.schedulePanel.Controls.Add(this.perfomancesCombobox);
             this.schedulePanel.Controls.Add(this.submitButton);
             this.schedulePanel.Controls.Add(this.cancelButton);
             this.schedulePanel.Controls.Add(this.headerLabel);
-            this.schedulePanel.Location = new System.Drawing.Point(12, 12);
+            this.schedulePanel.Location = new System.Drawing.Point(9, 10);
+            this.schedulePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.schedulePanel.Name = "schedulePanel";
-            this.schedulePanel.Size = new System.Drawing.Size(902, 559);
+            this.schedulePanel.Size = new System.Drawing.Size(676, 305);
             this.schedulePanel.TabIndex = 7;
-            // 
-            // headerLabel
-            // 
-            this.headerLabel.AutoSize = true;
-            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.headerLabel.Location = new System.Drawing.Point(402, 14);
-            this.headerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.headerLabel.Name = "headerLabel";
-            this.headerLabel.Size = new System.Drawing.Size(73, 25);
-            this.headerLabel.TabIndex = 2;
-            this.headerLabel.Text = "Action";
             // 
             // submitButton
             // 
             this.submitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(150)))));
             this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.submitButton.Location = new System.Drawing.Point(453, 476);
-            this.submitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.submitButton.Location = new System.Drawing.Point(337, 231);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(185, 70);
+            this.submitButton.Size = new System.Drawing.Size(139, 57);
             this.submitButton.TabIndex = 7;
             this.submitButton.Text = "Action";
             this.submitButton.UseVisualStyleBackColor = false;
@@ -124,25 +131,130 @@
             // 
             this.cancelButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(150)))));
             this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cancelButton.Location = new System.Drawing.Point(259, 476);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Location = new System.Drawing.Point(191, 231);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(185, 70);
+            this.cancelButton.Size = new System.Drawing.Size(139, 57);
             this.cancelButton.TabIndex = 6;
             this.cancelButton.Text = "Отмена";
             this.cancelButton.UseVisualStyleBackColor = false;
             // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.headerLabel.Location = new System.Drawing.Point(302, 11);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(60, 20);
+            this.headerLabel.TabIndex = 2;
+            this.headerLabel.Text = "Action";
+            // 
+            // perfomancesCombobox
+            // 
+            this.perfomancesCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomancesCombobox.FormattingEnabled = true;
+            this.perfomancesCombobox.Location = new System.Drawing.Point(110, 49);
+            this.perfomancesCombobox.Name = "perfomancesCombobox";
+            this.perfomancesCombobox.Size = new System.Drawing.Size(549, 28);
+            this.perfomancesCombobox.TabIndex = 8;
+            this.perfomancesCombobox.SelectedIndexChanged += new System.EventHandler(this.perfomancesCombobox_SelectedIndexChanged);
+            // 
+            // perfomancesLabel
+            // 
+            this.perfomancesLabel.AutoSize = true;
+            this.perfomancesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomancesLabel.Location = new System.Drawing.Point(13, 52);
+            this.perfomancesLabel.Name = "perfomancesLabel";
+            this.perfomancesLabel.Size = new System.Drawing.Size(91, 20);
+            this.perfomancesLabel.TabIndex = 9;
+            this.perfomancesLabel.Text = "Спектакль";
+            // 
+            // theatreLabel
+            // 
+            this.theatreLabel.AutoSize = true;
+            this.theatreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.theatreLabel.Location = new System.Drawing.Point(13, 88);
+            this.theatreLabel.Name = "theatreLabel";
+            this.theatreLabel.Size = new System.Drawing.Size(54, 20);
+            this.theatreLabel.TabIndex = 10;
+            this.theatreLabel.Text = "Театр";
+            // 
+            // theatreTextbox
+            // 
+            this.theatreTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.theatreTextbox.Location = new System.Drawing.Point(73, 85);
+            this.theatreTextbox.Name = "theatreTextbox";
+            this.theatreTextbox.Size = new System.Drawing.Size(586, 26);
+            this.theatreTextbox.TabIndex = 11;
+            // 
+            // perfomanceDateTimePicker
+            // 
+            this.perfomanceDateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.perfomanceDateTimePicker.Location = new System.Drawing.Point(162, 118);
+            this.perfomanceDateTimePicker.Name = "perfomanceDateTimePicker";
+            this.perfomanceDateTimePicker.Size = new System.Drawing.Size(497, 26);
+            this.perfomanceDateTimePicker.TabIndex = 12;
+            // 
+            // perfomanceDateLabel
+            // 
+            this.perfomanceDateLabel.AutoSize = true;
+            this.perfomanceDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceDateLabel.Location = new System.Drawing.Point(13, 123);
+            this.perfomanceDateLabel.Name = "perfomanceDateLabel";
+            this.perfomanceDateLabel.Size = new System.Drawing.Size(131, 20);
+            this.perfomanceDateLabel.TabIndex = 13;
+            this.perfomanceDateLabel.Text = "Дата спектакля";
+            // 
+            // perfomanceStartsLabel
+            // 
+            this.perfomanceStartsLabel.AutoSize = true;
+            this.perfomanceStartsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceStartsLabel.Location = new System.Drawing.Point(13, 155);
+            this.perfomanceStartsLabel.Name = "perfomanceStartsLabel";
+            this.perfomanceStartsLabel.Size = new System.Drawing.Size(117, 20);
+            this.perfomanceStartsLabel.TabIndex = 15;
+            this.perfomanceStartsLabel.Text = "Время начала";
+            // 
+            // perfomanceStartsDateTimePicker
+            // 
+            this.perfomanceStartsDateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceStartsDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.perfomanceStartsDateTimePicker.Location = new System.Drawing.Point(162, 150);
+            this.perfomanceStartsDateTimePicker.Name = "perfomanceStartsDateTimePicker";
+            this.perfomanceStartsDateTimePicker.Size = new System.Drawing.Size(497, 26);
+            this.perfomanceStartsDateTimePicker.TabIndex = 14;
+            // 
+            // perfomanceEndsLabel
+            // 
+            this.perfomanceEndsLabel.AutoSize = true;
+            this.perfomanceEndsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceEndsLabel.Location = new System.Drawing.Point(13, 187);
+            this.perfomanceEndsLabel.Name = "perfomanceEndsLabel";
+            this.perfomanceEndsLabel.Size = new System.Drawing.Size(142, 20);
+            this.perfomanceEndsLabel.TabIndex = 17;
+            this.perfomanceEndsLabel.Text = "Время окончания";
+            // 
+            // perfomanceEndsDateTimePicker
+            // 
+            this.perfomanceEndsDateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.perfomanceEndsDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.perfomanceEndsDateTimePicker.Location = new System.Drawing.Point(162, 182);
+            this.perfomanceEndsDateTimePicker.Name = "perfomanceEndsDateTimePicker";
+            this.perfomanceEndsDateTimePicker.Size = new System.Drawing.Size(497, 26);
+            this.perfomanceEndsDateTimePicker.TabIndex = 16;
+            // 
             // scheduleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(216)))));
-            this.ClientSize = new System.Drawing.Size(923, 583);
+            this.ClientSize = new System.Drawing.Size(692, 474);
             this.Controls.Add(this.schedulePanel);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.editButton);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.scheduleDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "scheduleForm";
             this.Text = "Расписание";
             this.Load += new System.EventHandler(this.scheduleForm_Load);
@@ -163,5 +275,15 @@
         private System.Windows.Forms.Label headerLabel;
         private System.Windows.Forms.Button submitButton;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label perfomancesLabel;
+        private System.Windows.Forms.ComboBox perfomancesCombobox;
+        private System.Windows.Forms.TextBox theatreTextbox;
+        private System.Windows.Forms.Label theatreLabel;
+        private System.Windows.Forms.Label perfomanceDateLabel;
+        private System.Windows.Forms.DateTimePicker perfomanceDateTimePicker;
+        private System.Windows.Forms.Label perfomanceEndsLabel;
+        private System.Windows.Forms.DateTimePicker perfomanceEndsDateTimePicker;
+        private System.Windows.Forms.Label perfomanceStartsLabel;
+        private System.Windows.Forms.DateTimePicker perfomanceStartsDateTimePicker;
     }
 }
--- Диплом/boxOffice/boxOffice/scheduleForm.cs
+++ Диплом/boxOffice/boxOffice/scheduleForm.cs
@@ -18,6 +18,8 @@ namespace boxOffice
             InitializeComponent();
         }
 
+        int theatreId;
+
         public void base_load()
         {
             DataSet ds = new DataSet();
@@ -36,11 +38,37 @@ namespace boxOffice
         {
             base_load();
             schedulePanel.Hide();
+            List<string> perfomances = new List<string>();
+            OleDbConnection con = staticVariables.con;
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand("select [Название спектакля] from Репертуар", con);
+                con.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    perfomances.Add(reader.GetString(0));
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось связаться с базой данных.");
+                con.Close();
+            }
+            for(int i = 0; i < perfomances.Count; i++)
+            {
+                perfomancesCombobox.Items.Add(perfomances[i]);
+            }
+            perfomancesCombobox.SelectedIndex = 0;
         }
 
         private void editButton_Click(object sender, EventArgs e)
         {
-
+            headerLabel.Text = "Редактировать";
+            submitButton.Text = "Изменить";
+            schedulePanel.Show();
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -48,6 +76,50 @@ namespace boxOffice
             headerLabel.Text = "Добавить";
             submitButton.Text = "Добавить";
             schedulePanel.Show();
+        }
+
+        private void perfomancesCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OleDbConnection con = staticVariables.con;
+            theatreId = 0;
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand("select [id театра], [Название спектакля] from Репертуар", con);
+                con.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    if(reader.GetString(1) == perfomancesCombobox.Text)
+                    {
+                        theatreId = reader.GetInt32(0);
+                    }
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось связаться с базой данных.");
+                con.Close();
+            }
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand("select Название from Театр where [id театра]=:theatreid", con);
+                cmd.Parameters.Add("theatreid", OleDbType.Integer).Value = theatreId;
+                con.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    theatreTextbox.Text = reader.GetString(0);
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось связаться с базой данных.");
+                con.Close();
+            }
         }
     }
 }
